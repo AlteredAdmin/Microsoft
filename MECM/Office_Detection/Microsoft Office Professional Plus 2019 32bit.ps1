5f4325bf1585eefc18448af8e876ac115172a1ea
Altered Admin <87214978+AlteredAdmin@users.noreply.github.com>
2024-04-09 08:04:36 -0400

    Delete MECM/Office_Detection directory

--- MECM/Office_Detection/Microsoft Office Professional Plus 2019 32bit.ps1
+++
@@ -1,3 +0,0 @@
-﻿$FilePath = "C:\Program Files (x86)\Microsoft Office\root\Office16\WINWORD.EXE"
-if(Test-Path $FilePath){$true}
-Else{$false}
--- MECM/Office_Detection/Microsoft Office Professional Plus 2019 64bit.ps1
+++
@@ -1,3 +0,0 @@
-﻿$FilePath = "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"
-if(Test-Path $FilePath){$true}
-Else{$false}
